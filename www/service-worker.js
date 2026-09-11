293a5d98e6a1add552cfdefddea7c8458be58894
Wizlock23 <gs75ghjw99@privaterelay.appleid.com>
2026-09-11 14:48:19 -0400

    Add files via upload
    13.2 Mobil fix

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_NAME='shattered-realms-v0.13.1';
+const CACHE_NAME='shattered-realms-v0.13.2';
 const CORE = [
   './','./index.html','./manifest.webmanifest','./battle/index.html',
   './css/app.css','./css/ui-v0120.css','./css/engagement-v0130.css','./css/battle-v0110.css','./css/battle-v0120.css','./css/battle-engagement-v0130.css',
