63ee85755d3a65d1c8da1fdf592c129a1afca3b3
Wizlock23 <gs75ghjw99@privaterelay.appleid.com>
2026-09-11 09:45:16 -0400

    Add files via upload
    New player experience

--- www/service-worker.js
+++ www/service-worker.js
@@ -1,10 +1,11 @@
-const CACHE_NAME = 'shattered-realms-v0.8.1';
+const CACHE_NAME = 'shattered-realms-v0.8.2';
 const CORE = [
   './', './index.html', './manifest.webmanifest',
   './css/app.css', './js/mobile-shell.js', './js/app.js', './js/pwa-install.js',
   './battle/index.html',
   './icons/apple-touch-icon.png', './icons/icon-192.png', './icons/icon-512.png',
-  './assets/campaign-ui-concept.png', './assets/faction-board.png', './assets/worldshard-behemoth.png'
+  './assets/campaign-ui-concept.png', './assets/faction-board.png', './assets/worldshard-behemoth.png',
+  './assets/intro/eryndor.png', './assets/intro/factions.png', './assets/intro/worldshard.png', './assets/intro/alfar.png'
 ];
 
 self.addEventListener('install', event => {
